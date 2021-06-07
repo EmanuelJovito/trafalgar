b18dac3916b2ad1ed15cef80bf4b84fdd8be24be
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-07 14:47:34 -0300

    feat: Criando landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Conteiner, Content } from '../styles/index'
+import { Conteiner, Content, Grid, OurServices } from '../styles/index'
 
 import { Header } from "../components/Header";
 
@@ -17,6 +17,46 @@ export default function Home() {
 
         <img src="trafalgar-header illustration 1.svg" alt="" />
       </Content>
+
+      <OurServices>
+        <h1>Our services</h1>
+        <div></div>
+        <p>We provide to you the best choiches for you. Adjust it to your health needs and make sure your undergo treatment
+        with our highly qualified doctors you can consult with us which type of service is suitable for your health</p>
+      </OurServices>
+
+      <Grid>
+        <div>
+          <img src="lupa.svg" alt="busca" />
+          <h1>Search doctor</h1>
+          <p>Choose your doctor from thousands of specialist, general, and trusted hospitals</p>
+        </div>
+        <div>
+          <img src="remedio.svg" alt="farmacia" />
+          <h1>Online pharmacy</h1>
+          <p>Buy  your medicines with our mobile application with a simple delivery system</p>
+        </div>
+        <div>
+          <img src="tablet.svg" alt="tablet" />
+          <h1>Consultation</h1>
+          <p>Free consultation with our trusted doctors and get the best recomendations</p>
+        </div>
+        <div>
+          <img src="info.svg" alt="informação" />
+          <h1>Details info</h1>
+          <p>Free consultation with our trusted doctors and get the best recomendations</p>
+        </div>
+        <div>
+          <img src="kit.svg" alt="emergencia" />
+          <h1>Emergency care</h1>
+          <p>You can get 24/7 urgent care for yourself or your children and your lovely family</p>
+        </div>
+        <div>
+          <img src="tabela.svg" alt="tabela" />
+          <h1>Tracking</h1>
+          <p>Track and save your medical history and health data </p>
+        </div>
+      </Grid>
     </Conteiner>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -57,3 +57,63 @@ export const Content = styled.div`
     width: 35rem;
   }
 `
+
+export const OurServices = styled.div`
+  width: 55%;
+
+  margin-top: 10rem;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  div {
+    width: 5rem;
+    border-bottom: 1px solid black;
+    margin-top: 1.5rem;
+  }
+
+  p {
+    margin-top: 1.5rem;
+    text-align: center;
+    color: rgba(125, 121, 135, 1);
+  }
+`
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 2rem;
+
+  margin-top: 4rem;
+  
+  div {
+    width: 21rem;
+    height: 22rem;
+
+    padding: 2rem;
+
+    background: rgba(255, 255, 255, 1);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    border: 1px solid black;
+    border-radius: 20px;
+
+    img {
+      width: 4rem;
+    }
+
+    h1 {
+      margin-top: 3rem;
+      font-size: 1.5rem;
+    }
+
+    p {
+      margin-top: 1rem;
+      line-height: 28px;
+    }
+  }
+`
