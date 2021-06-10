8cfc0e53d8d44d2d643fdb4d1c0fcd9fb540748d
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-10 14:08:51 -0300

    feat: Adicionando titulo da pagina

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title>Trafalgar | Home</title>
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;700&display=swap" rel="stylesheet" />
         </Head>
