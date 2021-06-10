77546515770e021d42adf51b3cfd3f968b893c8c
EmanuelJovito <emanuel_carneiro@hotmail.com>
2021-06-10 11:36:22 -0300

    feat: Finalizando landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
-import { Conteiner, Content, Grid, OurServices } from '../styles/index'
+import { Conteiner, Content, Grid, OurServices, Button, Leading, Download, GridImages, GridTitle } from '../styles/index'
 
 import { Header } from "../components/Header";
+import { Slider } from '../components/Slider';
+import { Footer } from '../components/Footer';
 
 export default function Home() {
   return (
@@ -56,7 +58,90 @@ export default function Home() {
           <h1>Tracking</h1>
           <p>Track and save your medical history and health data </p>
         </div>
+
       </Grid>
+
+      <Button>
+        Learn more
+      </Button>
+      
+      <Leading>
+        <img src="image2.svg" alt="image2" />
+        <div>
+          <h1>Leading healthcare providers</h1>
+          <p>Trafalgar provides progressive, and affordable healthcare, accessible on mobile and online for everyone. To 
+          us, it’s not just work. We take pride 
+          in the solutions we deliver</p>
+
+          <Button>
+            Learn more
+          </Button>
+        </div>
+      </Leading>
+
+      <Download>
+        <div>
+          <h1>Download our mobile apps</h1>
+          <p>Our dedicated patient engagement app and web portal allow you to 
+          access information instantaneously (no tedeous form, long calls, 
+          or administrative hassle) and securely</p>
+
+          <Button>
+            Download
+            <img src="download.svg" alt="download" />
+          </Button>
+        </div>
+
+        <img src="image3.svg" alt="image3" className='contentImage'/>
+      </Download>
+
+      <Slider />
+      
+      <GridTitle>
+        <h1>Check out our latest article</h1>
+        <div></div>
+      </GridTitle>
+      <GridImages>
+        <div>
+          <img src="gridImage1.png" alt="image1" />   
+          <div className="textDiv">
+            <h1>Disease detection, check up in the laboratory</h1>
+            <p>In this case, the role of the health laboratory is very important to do
+            a disease detection...</p>
+            <button>
+              Read more
+              <img src="seta2.svg" alt="seta direita" />
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src="gridImage2.png" alt="image2" />
+          <div className="textDiv">
+            <h1>Herbal medicines that are safe for consumption</h1>
+            <p>Herbal medicine is very widely used at this time because of its very good for your health...</p>
+            <button>
+              Read more
+              <img src="seta2.svg" alt="seta direita" />
+            </button>
+          </div>
+        </div>
+        <div>
+          <img src="gridImage3.png" alt="image3" />    
+          <div className="textDiv">
+            <h1>Natural care for healthy facial skin</h1>
+            <p>A healthy lifestyle should start from now and also for your skin health.
+            There are some...</p>
+            <button>
+              Read more
+              <img src="seta2.svg" alt="seta direita" />
+            </button>
+          </div>
+        </div>
+      </GridImages>
+
+      <Button>View all</Button>
+
+      <Footer />
     </Conteiner>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -50,6 +50,10 @@ export const Content = styled.div`
 
       border: none;
       border-radius: 55px;
+
+      :hover {
+        background: rgba(69, 143, 246, 0.8)
+      }
     }
   }
 
@@ -99,7 +103,6 @@ export const Grid = styled.div`
     flex-direction: column;
     justify-content: center;
 
-    border: 1px solid black;
     border-radius: 20px;
 
     img {
@@ -114,6 +117,200 @@ export const Grid = styled.div`
     p {
       margin-top: 1rem;
       line-height: 28px;
+    }
+  }
+`
+
+export const Button = styled.button`
+  margin-top: 4rem;
+  width: 12.5rem;
+  height: 3.5rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: transparent;
+
+  color: #458FF6;
+  font-weight: 700;
+
+  border: 1px solid #458FF6;
+  border-radius: 55px;
+
+  img {
+    margin-left: 0.5rem;
+  }
+
+  :hover {
+    background: rgba(255, 255, 255, 0.5);
+  }
+`
+
+export const Leading = styled.div`
+  width: 71%;
+
+  margin-top: 10rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  justify-content: space-around;
+
+  div {
+    width: 22rem;
+
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      position: relative;
+      padding-bottom: 2rem;
+
+      &::after {
+        width: 3rem;
+        height: 2px;
+
+        content: '';
+
+        background-color: #000;
+
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    }
+
+    p {
+      margin-top: 1.5rem;
+    }
+  }
+
+  img {
+    width: 35rem;
+  }
+`
+
+export const Download = styled.div`
+  width: 71%;
+
+  margin-top: 10rem;
+
+  display: flex;
+  align-items: center;
+
+  justify-content: space-around;
+
+  div {
+    width: 22rem;
+
+    display: flex;
+    flex-direction: column;
+
+    h1 {
+      position: relative;
+      padding-bottom: 2rem;
+
+      &::after {
+        width: 3rem;
+        height: 2px;
+
+        content: '';
+
+        background-color: #000;
+
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        right: 0;
+      }
+    }
+
+    p {
+      margin-top: 1.5rem;
+    }
+  }
+`
+
+export const GridTitle = styled.div`
+  margin-top: 7rem;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  div {
+    width: 4rem;
+    height: 2px;
+
+    margin-top: 2rem;
+
+    background: #000;
+  }
+`
+
+export const GridImages = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  column-gap: 2rem;
+
+  margin-top: 5rem;
+
+  & > div {
+    width: 21rem;
+    height: 32rem;
+
+    background: rgba(255, 255, 255, 1);
+    border-radius: 20px; 
+
+    & > img {
+      width: 100%;
+      border-radius: 20px 20px 0 0;
+    }
+
+    .textDiv {
+      width: 90%;
+
+      margin-top: 1rem;
+      margin-left: 2rem;
+
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+
+      h1 {
+        width: 17rem;
+        font-size: 1.3rem;
+        line-height: 32px;
+      }
+
+      p {
+        width: 17rem;
+        margin-top: 0.5rem;
+        margin-bottom: 3rem;
+        line-height: 28px;
+      }
+
+      button {
+        width: 70%;
+
+        border: none;
+        background: none;
+
+        display: flex;
+        align-items: center;
+
+        color: rgba(64, 137, 237, 1);
+        font-weight: 600;
+
+        img {
+          width: 1rem;
+          margin-top: 0.2rem;
+          margin-left: 1rem;
+        }
+      }
     }
   }
 `
